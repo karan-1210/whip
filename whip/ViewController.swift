3af843047c12b7387e27a9a04e4ed27d9b61fb9e
karan khandelwal <karantambi00@gmail.com>
2020-08-27 21:24:42 +0530

    Initial Commit

---
+++ whip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whip
+//
+//  Created by Karan Khandelwal on 8/27/20.
+//  Copyright © 2020 Karan Khandelwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
